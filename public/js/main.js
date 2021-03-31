c13e8e891f72f0c121b78c10a081af33fd82cf44
Alex <variuhin@yandex.ru>
2021-03-31 16:43:22 +0300

    Add game loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,22 @@
-import {createBalloonSvg} from './createBalloonSvg.js'
+import {Balloon} from './classes/Balloon.js'
 
 const svg = document.querySelector('.game-svg')
-svg.append(createBalloonSvg())
+const balloon = new Balloon(svg)
+
+const deltaTime = 1/60
+let prevTime = 0
+let accumulatedTime = 0
+
+function game(time){
+  accumulatedTime += (time - prevTime) / 1000
+  prevTime = time
+
+  while(accumulatedTime > deltaTime){
+    balloon.update(deltaTime)
+    accumulatedTime -= deltaTime
+  }
+
+  requestAnimationFrame(game)
+}
+
+requestAnimationFrame(game)
