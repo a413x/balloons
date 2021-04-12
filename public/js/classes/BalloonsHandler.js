f55641a4c300be98a99419b1bf16fa087346b160
Alex <variuhin@yandex.ru>
2021-04-12 16:30:33 +0300

    Separate balloons handling into class

---
+++ public/js/classes/BalloonsHandler.js
@@ -0,0 +1,46 @@
+import {Balloon} from './Balloon.js'
+import {collisionDetect} from '../collision.js'
+export class BalloonsHandler{
+  constructor(ctx){
+    this.ctx = ctx
+    this.balloons = new Map()
+    this.timeToNextBalloon = 0
+  }
+  createBalloon(speedCoef){
+    const balloon = new Balloon(this.ctx, speedCoef)
+    this.balloons.set(balloon.id, balloon)
+  }
+  balloonDestroy(id){
+    this.balloons.get(id).destroy()
+    this.balloons.delete(id)
+  }
+  balloonsCreateInterval(deltaTime, params, enable){
+    if(!enable) return
+    this.timeToNextBalloon += deltaTime
+    if(this.timeToNextBalloon > params.nextBalloonTime){
+      this.createBalloon(params.speedCoef)
+      this.timeToNextBalloon = 0
+    }
+  }
+  updateBalloons(deltaTime, wind, score){
+    this.balloons.forEach(b => b.update(
+      deltaTime,
+      wind.calculateWindAffect(b),
+      id => {
+        score.update(false)
+        this.balloonDestroy(id)
+      }
+    ))
+  }
+  checkCollisions(nail, score){
+    const collidingIds = collisionDetect(
+      nail.getNailPoint(), this.balloons
+    )
+    if(collidingIds.length){
+      collidingIds.forEach(id => {
+        this.balloonDestroy(id)
+        score.update(true)
+      })
+    }
+  }
+}
--- public/js/classes/Game.js
+++ public/js/classes/Game.js
@@ -1,81 +1,39 @@
-import {Balloon} from './Balloon.js'
+import {BalloonsHandler} from './BalloonsHandler.js'
 import {Nail} from './Nail.js'
 import {Wind} from './Wind.js'
 import {Score} from './Score.js'
 import {Level} from './Level.js'
 import {Timer} from './Timer.js'
-import {collisionDetect} from '../collision.js'
 
 export class Game{
   constructor(){
     this.ctx = document.querySelector('.game-svg')
-    this.balloons = new Map()
     this.nail = new Nail(this.ctx)
     this.wind = new Wind(this.ctx)
     this.wind.addWind()
     this.score = new Score()
-
-    this.timeToNextBalloon = 0
-
     this.level = new Level()
     this.timer = new Timer()
+    this.balloonsHandler = new BalloonsHandler(this.ctx)
   }
-
-  createBalloon(){
-    const balloon = new Balloon(this.ctx, this.level.params.speedCoef)
-    this.balloons.set(balloon.id, balloon)
-  }
-
-  balloonsCreateInterval(deltaTime){
-    if(this.levelOver) return
-    this.timeToNextBalloon += deltaTime
-    if(this.timeToNextBalloon > this.level.params.nextBalloonTime){
-      this.createBalloon()
-      this.timeToNextBalloon = 0
-    }
-  }
-
-  balloonDestroy(id){
-    this.balloons.get(id).destroy()
-    this.balloons.delete(id)
-  }
-
   endLevel(){
     this.levelOver = true
-    if(this.balloons.size === 0){
+    if(this.balloonsHandler.balloons.size === 0){
       this.levelOver = false
       this.levelChange()
     }
   }
-
   levelChange(){
     this.score.reset()
     this.level.nextLevel()
     this.timer.enable = true
   }
-
   update(deltaTime){
-    this.balloons.forEach(b => b.update(
-      deltaTime,
-      this.wind.calculateWindAffect(b),
-      id => {
-        this.score.update(false)
-        this.balloonDestroy(id)
-      }
-    ))
-
-    const collidingIds = collisionDetect(
-      this.nail.getNailPoint(), this.balloons
+    this.balloonsHandler.updateBalloons(deltaTime, this.wind, this.score)
+    this.balloonsHandler.checkCollisions(this.nail, this.score)
+    this.balloonsHandler.balloonsCreateInterval(
+      deltaTime, this.level.params, !this.levelOver
     )
-
-    if(collidingIds.length){
-      collidingIds.forEach(id => {
-        this.balloonDestroy(id)
-        this.score.update(true)
-      })
-    }
-
     this.timer.update(deltaTime, () => this.endLevel())
-    this.balloonsCreateInterval(deltaTime)
   }
 }
