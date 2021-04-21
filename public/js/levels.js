282f30280cfdc1fe92965c74731b30d7ddec642c
Alex <variuhin@yandex.ru>
2021-04-21 15:52:39 +0300

    Adjust levels

--- public/js/levels.js
+++ public/js/levels.js
@@ -1,7 +1,9 @@
 export const levels = [
-  {speedCoef: 5000, nextBalloonTime: 3},
-  {speedCoef: 7000, nextBalloonTime: 2},
-  {speedCoef: 9000, nextBalloonTime: 1},
-  {speedCoef: 12000, nextBalloonTime: .5},
-  {speedCoef: 15000, nextBalloonTime: .3},
+  {speedCoef: 7000, nextBalloonTime: 2.4, balloonsToLoose: 2},
+  {speedCoef: 9000, nextBalloonTime: 2, balloonsToLoose: 3},
+  {speedCoef: 10000, nextBalloonTime: 1, balloonsToLoose: 5},
+  {speedCoef: 11000, nextBalloonTime: .8, balloonsToLoose: 6},
+  {speedCoef: 11200, nextBalloonTime: .5, balloonsToLoose: 8},
+  {speedCoef: 11500, nextBalloonTime: .5, balloonsToLoose: 10},
+  {speedCoef: 11500, nextBalloonTime: .3, balloonsToLoose: 14},
 ]
