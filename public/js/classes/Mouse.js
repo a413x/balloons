56de56ca4ae57a18309b6faad930a84427fae627
Alex <variuhin@yandex.ru>
2021-04-22 17:20:37 +0300

    Change mouse handle and add touch support

--- public/js/classes/Mouse.js
+++ public/js/classes/Mouse.js
@@ -6,12 +6,30 @@ export class Mouse{
     }
   }
   handleEnent(e){
-    this.callback( { x: e.offsetX, y: e.offsetY} )
+    const event = e.targetTouches ? e.targetTouches[0] : e
+    const zoom = getComputedStyle(document.body).getPropertyValue('zoom')
+    let obj = e.target
+    let objLeft = 0
+    let objTop = 0
+    let x = event.pageX/zoom
+    let y = event.pageY/zoom
+    while (obj.offsetParent)
+    {
+      objLeft += obj.offsetLeft
+      objTop += obj.offsetTop
+      obj = obj.offsetParent
+    }
+    x -= objLeft
+    y -= objTop
+    this.callback({x, y})
   }
   listen(obj){
-    obj.onmousemove = e => this.handleEnent(e)
+    const handle = e => this.handleEnent(e)
+    obj.onmousemove = handle
+    obj.ontouchmove = handle
   }
   detach(obj){
     obj.onmousemove = null
+    obj.ontouchmove = null
   }
 }
